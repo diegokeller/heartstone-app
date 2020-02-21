8b9dcc90f1319364695d2dfcc20b31e459533a88
Diego Keller <keller.diego@gmail.com>
2020-02-21 17:28:02 -0300

    Implementing component card-list

--- src/app/tabs/card/components/card-list.component.ts
+++ src/app/tabs/card/components/card-list.component.ts
@@ -1,9 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-card-list',
     templateUrl: './card-list.component.html'
 })
 export class CardListComponent {
-    
+
+    @Input() items: any[] = []
+    @Input() listName: string
+
 }
